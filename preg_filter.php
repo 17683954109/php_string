07bc226c7ce4714150eff194fd9b3b6ac806d690
zjz <17683954109@163.com>
2018-10-25 13:52:03 +0800

    init

---
+++ preg_filter.php
@@ -0,0 +1,61 @@
+<?php
+$str = <<<EOT
+dsdad127.0.0.1dfsdadads
+sdef125.116.52.3sdsds
+48554dsad45848dsd4saa8
+EOT;
+$reg = '/([0-9]{1,3}\.{1}){3}[0-9]{1}/';
+$res = preg_filter($reg,'这是ip',$str);
+var_dump($res);
+/*
+ string(65) "dsdad这是ipdfsdadads  -> 原本是 ip 的地方全部替换成了我指定的 ‘这是ip’
+    sdef这是ipsdsds
+    48554dsad45848dsd4saa8"
+ *
+ * */
+
+//  -------------------------------------------------------------->  数组的匹配
+
+//  php.net 上的示例代码解析:
+
+$subject = array('1', 'a', '2', 'b', '3', 'A', 'B', '4');
+$pattern = array('/\d/', '/[a-z]/', '/[1a]/');
+$replace = array('A:$0', 'B:$0', 'C:$0');
+
+echo "preg_filter returns\n";
+print_r(preg_filter($pattern, $replace, $subject));
+/*
+ * preg_filter:
+ *
+ * 第一轮 ： \d  替换为 A: 加上它匹配到的第0条结果
+ * A:1 , a , A:2 , b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 第二轮 : [a-z] 替换为 B: 加上它匹配到的第0条结果
+ * A:1 , B:a , A:2 , B:b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 第三轮 : [1a] 替换为 C: 加上它匹配到的第0条结果
+ * A:C:1 , B:C:a , A:2 , B:b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 结果: 去除掉未匹配和修改的值
+ * A:C:1 , B:C:a , A:2 , B:b , A:3 , A:4
+ *
+ * */
+
+echo "preg_replace returns\n";
+print_r(preg_replace($pattern, $replace, $subject));
+/*
+ * preg_replace:
+ *
+ * 第一轮 ： \d  替换为 A: 加上它匹配到的第0条结果
+ * A:1 , a , A:2 , b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 第二轮 : [a-z] 替换为 B: 加上它匹配到的第0条结果
+ * A:1 , B:a , A:2 , B:b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 第三轮 : [1a] 替换为 C: 加上它匹配到的第0条结果
+ * A:C:1 , B:C:a , A:2 , B:b , A:3 , A , B , A:4    --> 未匹配和修改: A , B
+ *
+ * 结果: 不去除未匹配和修改的值
+ * A:C:1 , B:C:a , A:2 , B:b , A:3 , A , B , A:4
+ *
+ * */
---
+++ preg_match.php
@@ -0,0 +1,20 @@
+<?php
+$str = <<<EOT
+dsdad127.0.0.1dfsdadads
+sdef125.116.52.3sdsds
+48554dsad45848dsd4saa8
+EOT;
+$reg = '/([0-9]{1,3}\.{1}){3}[0-9]{1}/m';
+$res = preg_match($reg,$str);
+var_dump(boolval($res));    //  bool(true)  -> 匹配的结果
+$res = preg_match($reg,$str,$arr);
+var_dump($arr);
+/*
+ *
+ array(2) {
+  [0] =>
+  string(9) "127.0.0.1"   -> 匹配到的字符
+  [1] =>
+  string(2) "0."    -> 子表达式匹配到的字符: 由于是使用了量词，所以只返回最后一次的,完整的应该为 ：127.0.0.
+}
+ * */
